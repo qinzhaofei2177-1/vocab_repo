94731c95ac5bac363645fac04451254dd4166cd8
qinzhaofei2177-1 <qinzhaofei2177@163.com>
2020-08-28 16:10:21 +0800

    Update forms.py

--- vocab/vocabexam/forms.py
+++ vocab/vocabexam/forms.py
@@ -1,3 +1,6 @@
 from django import forms
 
 
+class TestForm(forms.Form):
+    CHOICES = [('1', 'First'), ('2', 'Second')]
+    option = forms.ChoiceField(label='Word', widget=forms.RadioSelect, choices=CHOICES)
